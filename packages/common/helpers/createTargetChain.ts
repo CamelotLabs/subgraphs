7aef8231e0b7c64f50caa3279a328c573fd91179
0xCaliburSigma <0xcalibursigma@protonmail.com>
2024-11-07 11:05:56 -0400

    use getAddress for all target chain addrs

--- packages/common/helpers/createTargetChain.ts
+++ packages/common/helpers/createTargetChain.ts
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url'
 import path from "path"
 import ChainInfo from "./chainInfo.ts"
 import dotenv from "dotenv"
+import { ethers } from "ethers"
 
 const getCurrentDir = () => {
   const __filename = fileURLToPath(import.meta.url)
@@ -26,11 +27,11 @@ const network = subgraphConfig?.network || "arbitrum-one"
 
 // AMM V2 PARAMS
 const ammv2Name = config?.subgraphs.ammV2.name
-const factoryV2 = config?.contracts.factoryV2
-const wrappedNativeV2 = config?.assets.native.address
-const wrappedNativeUSDCPoolV2 = config?.assets.nativePairV2
+const factoryV2 = ethers.getAddress(config?.contracts.factoryV2)
+const wrappedNativeV2 = ethers.getAddress(config?.assets.native.address)
+const wrappedNativeUSDCPoolV2 = ethers.getAddress(config?.assets.nativePairV2)
 const whitelistTokensV2 = subgraphConfig?.whitelistTokensV2
-const stableCoin = config?.assets.stable
+const stableCoin = ethers.getAddress(config?.assets.stable)
 const minimumUSDThresholdNewPairs = subgraphConfig?.minimumUSDThresholdNewPairs
 const minimumLiquidityThresholdETHV2 = subgraphConfig?.minimumLiquidityThresholdETHV2
 const minimumLiquidityETH = subgraphConfig?.minimumLiquidityETH
@@ -38,10 +39,10 @@ const startBlockAmmV2 = subgraphConfig?.startBlockAmmV2
 
 // AMM V3 PARAMS
 const ammv3Name = config?.subgraphs.ammV3.name
-const factoryV3 = config?.contracts.factoryV3
-const nftPositionManagerV3 = config?.contracts.nftPositionManagerV3
-const wrappedNativeV3 = config?.assets.native.address
-const wrappedNativeUSDCPoolV3 = config?.assets.nativePairV3
+const factoryV3 = ethers.getAddress(config?.contracts.factoryV3)
+const nftPositionManagerV3 = ethers.getAddress(config?.contracts.nftPositionManagerV3)
+const wrappedNativeV3 = ethers.getAddress(config?.assets.native.address)
+const wrappedNativeUSDCPoolV3 = ethers.getAddress(config?.assets.nativePairV3)
 const minimumLiquidityThresholdETHV3 = subgraphConfig?.minimumLiquidityThresholdETHV3
 const whitelistTokensV3 = subgraphConfig?.whitelistTokensV3
 const stableCoins = subgraphConfig?.stableCoins
@@ -50,8 +51,8 @@ const startBlockAmmV3 = subgraphConfig?.startBlockAmmV3
 
 // INCENTIVES PARAMS
 const incentivesName = config?.subgraphs.incentives.name
-const campaignFactory = config?.contracts.campaignFactory
-const distributor = config?.contracts.distributor
+const campaignFactory = ethers.getAddress(config?.contracts.campaignFactory)
+const distributor = ethers.getAddress(config?.contracts.distributor)
 const startBlockIncentives = subgraphConfig?.startBlockIncentives
 
 // BLOCKS PARAMS
