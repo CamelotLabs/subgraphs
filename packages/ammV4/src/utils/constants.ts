a26b529a0af069dac931b32f25dcf64f8c922cb0
0xpercival <percival@camelot.fi>
2024-12-06 15:29:18 +0100

    add ammV4

---
+++ packages/ammV4/src/utils/constants.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prefer-const */
+import { BigInt, BigDecimal, Address, bigDecimal } from '@graphprotocol/graph-ts'
+import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
+
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = '0xDfCC62B3eE40EE453Cb2788576B56e746FC79827'
+export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)
+
+export let factoryContract = FactoryContract.bind(Address.fromString(FACTORY_ADDRESS))
+
+export let pools_list = [""]
